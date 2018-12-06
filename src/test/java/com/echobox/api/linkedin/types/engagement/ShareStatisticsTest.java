2ae695d2d743182eb90ba9e3221b1f5d6bc311db
clementEchobox <clement@echobox.com>
2018-12-06 10:08:28 +0000

    TEC-6427 : create linkedIn api follower statistics objects (#12)
    * TEC-4766: added linkedin engagement types
    
    * TEC-4766: added test
    
    * TEC-4766: renaming
    
    * TEC-4766: use linked annotation and the DefaultJsonMapper
    
    * TEC-6427: created Statistics POJOs
    
    * TEC-6427: renamed fields, and added test properly
    
    * TEC-6427: checkStyle
    
    * Merge branch 'master' of https://github.com/ebx/ebx-linkedin-sdk into TEC-6427-CreateLinkedinAPIFollowerStatisticsObjects
    
    * 'master' of https://github.com/ebx/ebx-linkedin-sdk:
      TEC-6259 extend linkedin api client for delete (#7)
      TEC-4768 create linkedin share type (#6)
      TEC-4766 : create linked in engagement pojo (#10)
    
    # Conflicts:
    #	src/main/java/com/echobox/api/linkedin/types/engagement/Share.java
    #	src/test/java/com/echobox/api/linkedin/types/engagement/ShareTest.java
    
    * TEC-6427: suggested changes
    
    * TEC-6427 : fixing wrong comments
    
    * TEC-6427 : locale, not Share Text

--- src/test/java/com/echobox/api/linkedin/types/engagement/ShareStatisticsTest.java
+++ src/test/java/com/echobox/api/linkedin/types/engagement/ShareStatisticsTest.java
@@ -18,6 +18,8 @@
 package com.echobox.api.linkedin.types.engagement;
 
 import com.echobox.api.linkedin.jsonmapper.DefaultJsonMapper;
+import com.echobox.api.linkedin.jsonmapper.DefaultJsonMapperTestBase;
+import com.echobox.api.linkedin.types.Paging;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -27,7 +29,7 @@ import java.util.List;
 /**
  * @author clementcaylux on 27/11/2018.
  */
-public class ShareTest {
+public class ShareStatisticsTest extends DefaultJsonMapperTestBase {
 
   /**
    * test that an engagement JSON is correctly deserialized
@@ -37,7 +39,7 @@ public class ShareTest {
   @Test
   public void testEngagementJson() {
     DefaultJsonMapper mapper = new DefaultJsonMapper();
-    Share shareStatistics = mapper.toJavaObject(
+    ShareStatistics shareStatistics = mapper.toJavaObject(
         "{\n" + "  \"elements\": [\n" + "    {\n" + "      \"totalShareStatistics\": {\n"
             + "        \"shareCount\": 5,\n" + "        \"clickCount\": 78,\n"
             + "        \"engagement\": 0.022886324947985624,\n" + "        \"likeCount\": 14,\n"
@@ -51,7 +53,7 @@ public class ShareTest {
             + "      \"share\": \"urn:li:share:1000001\",\n"
             + "      \"organizationalEntity\": \"urn:li:organization:2414183\"\n" + "    }\n"
             + "  ],\n" + "  \"paging\": {\n" + "    \"count\": 10,\n" + "    \"start\": 0,\n"
-            + "    \"links\": []\n" + "  }\n" + "}", Share.class);
+            + "    \"links\": []\n" + "  }\n" + "}", ShareStatistics.class);
 
     Paging paging = shareStatistics.getPaging();
     
