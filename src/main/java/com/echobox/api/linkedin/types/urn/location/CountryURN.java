9118d36764e6c73df6d8bbc2f95ae29ef91c944e
Unknown <eddspencer@gmail.com>
2018-12-12 07:38:07 +0000

    TEC-6464 Comment fixing

--- src/main/java/com/echobox/api/linkedin/types/urn/location/CountryURN.java
+++ src/main/java/com/echobox/api/linkedin/types/urn/location/CountryURN.java
@@ -25,7 +25,7 @@ import lombok.Getter;
 /**
  * Country URN POJO
  * @see <a href="https://developer.linkedin.com/docs/ref/v2/standardized-data/locations/countries">
- * Country ContainsURN</a>
+ * Country URN</a>
  * @author joanna
  *
  */
@@ -39,7 +39,7 @@ public class CountryURN extends LocaleStringNameURN {
   private String countryCode;
   
   /**
-   * The country group ContainsURN that this country belongs to
+   * The country group URN that this country belongs to
    */
   @Getter
   @LinkedIn
