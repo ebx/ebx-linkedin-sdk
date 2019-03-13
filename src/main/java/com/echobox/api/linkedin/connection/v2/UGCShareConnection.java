e2b8be9b621fdfd5a0faa8e9f0379694a280b2e0
Joanna <joanna@echobox.com>
2019-03-13 10:44:48 +0000

    TEC-6466 checkstyle

--- src/main/java/com/echobox/api/linkedin/connection/v2/UGCShareConnection.java
+++ src/main/java/com/echobox/api/linkedin/connection/v2/UGCShareConnection.java
@@ -96,6 +96,7 @@ public class UGCShareConnection extends ConnectionBaseV2 {
    * community-management/shares/ugc-post-api#retrieve-ugc-posts-by-authors">Retrieve UGC Posts
    * by authors</a>
    * @param authorURN the author URN - can be either an organization or person URN
+   * @param count the number of entries to be returned per paged request
    * @return the connection of UCG share which is paged
    */
   public Connection<UGCShare> retrieveUGCPostsByAuthors(URN authorURN, Integer count) {
