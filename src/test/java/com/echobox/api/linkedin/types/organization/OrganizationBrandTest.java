c1f4ad1de21b68ee7b8e3c42e9a95b6508d8e91a
Alexandros Milaios <alexmilaios@gmail.com>
2020-10-20 15:38:24 +0100

    Release To Maven  1.0.9 version (#109)
    * Bump dev from version 1.0.6 to 1.0.7 (#101)
    
    * Bump dev from version 1.0.6 to 1.0.7
    
    * TEC-9768 update release notes
    
    * TEC-10202 Ensure retrieving UGC are optionally sorted
    
    * TEC-10202 Removing bad pom changes.
    
    * TEC-10202 Changelog updated
    
    * TEC-10528 Update LinkedIn SDK to include new organization brand page endpoints (#107)
    
    * TEC-10528 Retrieve OrganizationBrands by parent Organization
    
    * TEC-10528 unit tests and a method to get organizationBrand from id
    
    * TEC-10528 retrieve organizationBrand by id
    
    * TEC-10528 update minor version
    
    * Revert "TEC-10528 update minor version"
    
    * TEC-10528 update change log
    
    * TEC-10528 update version
    
    * TEC-10528 Update LinkedIn SDK to include new organization brand page endpoints (#110)
    
    * TEC-10528 Retrieve OrganizationBrands by parent Organization
    
    * TEC-10528 unit tests and a method to get organizationBrand from id
    
    * TEC-10528 retrieve organizationBrand by id
    
    * TEC-10528 update minor version
    
    * Revert "TEC-10528 update minor version"
    
    * TEC-10528 update change log
    
    * TEC-10528 update version
    
    * TEC-10528 Code review changes
    
    * TEC-10528 Code review changes
    
    * TEC-10528 fix pom version
    
    Co-authored-by: diepjy <diep.j95@gmail.com>
    Co-authored-by: chris-ebx <58697895+ChrisEchobox@users.noreply.github.com>
    Co-authored-by: chris-ebx <58697895+chris-ebx@users.noreply.github.com>

---
+++ src/test/java/com/echobox/api/linkedin/types/organization/OrganizationBrandTest.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.echobox.api.linkedin.types.organization;
+
+import static org.junit.Assert.assertEquals;
+
+import com.echobox.api.linkedin.jsonmapper.DefaultJsonMapper;
+import com.echobox.api.linkedin.jsonmapper.DefaultJsonMapperTestBase;
+import org.junit.Test;
+
+/**
+ * The type Organization brand test.
+ * @author alexandros
+ */
+public class OrganizationBrandTest extends DefaultJsonMapperTestBase {
+  
+  /**
+   * Test organization brand json can be deserialized.
+   */
+  @Test
+  public void testOrganizationBrandJsonCanBeDeserialized() {
+    String json = readFileToString(
+        "com.echobox.api.linkedin.jsonmapper/organizationBrand.json");
+  
+    DefaultJsonMapper defaultJsonMapper = new DefaultJsonMapper();
+  
+    OrganizationBrand organizationBrand = 
+        defaultJsonMapper.toJavaObject(json, OrganizationBrand.class);
+  
+    assertEquals(11111111L, organizationBrand.getId());
+    assertEquals("example.com", organizationBrand.getLocalizedName());
+    
+    assertEquals("urn:li:media:/p/8/000/206/3de/1111111.png", 
+        organizationBrand.getLogo().getCropped().toString());
+    assertEquals("urn:li:media:/p/8/000/206/3de/1111111.png",
+        organizationBrand.getLogo().getOriginal().toString());
+    assertEquals(0, organizationBrand.getLogo().getCropInfo().getHeight());
+    assertEquals(0, organizationBrand.getLogo().getCropInfo().getWidth());
+    assertEquals(0, organizationBrand.getLogo().getCropInfo().getXAxis());
+    assertEquals(0, organizationBrand.getLogo().getCropInfo().getYAxis());
+    
+    assertEquals("test_product", organizationBrand.getName().getLocalized().get("en_US"));
+    assertEquals("US", organizationBrand.getName().getPreferredLocale().getCountry());
+    assertEquals("en", organizationBrand.getName().getPreferredLocale().getLanguage());
+    
+    assertEquals("example.com", organizationBrand.getVanityName());
+    
+    assertEquals("2", organizationBrand.getVersionTag());
+    
+    assertEquals("urn:li:organization:2222222", 
+        organizationBrand.getParentRelationship().getParent().toString());
+  }
+  
+}
