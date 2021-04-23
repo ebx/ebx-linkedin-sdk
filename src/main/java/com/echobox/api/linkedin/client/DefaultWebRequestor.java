d0565cf58c627f9407a522e64aeb5f946948f914
Dercio Daio <daio.dercio2015@gmail.com>
2021-04-23 14:14:32 +0100

    GH-123 Add application/x-wwww-form-urlenconded header to obtain access token request and update additional header method to add headers as a list.

--- src/main/java/com/echobox/api/linkedin/client/DefaultWebRequestor.java
+++ src/main/java/com/echobox/api/linkedin/client/DefaultWebRequestor.java
@@ -55,6 +55,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -634,9 +635,7 @@ public class DefaultWebRequestor implements WebRequestor {
     if (headers != null) {
       // Add any additional headers
       for (String headerKey : headers.keySet()) {
-        if (!headerKey.equalsIgnoreCase("content-type")) {
-          httpHeaders.put(headerKey, headers.get(headerKey));
-        }
+        httpHeaders.putIfAbsent(headerKey, Collections.singletonList(headers.get(headerKey)));
       }
     }
   
