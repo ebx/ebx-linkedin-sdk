01abf002978a92bd3701dee61a435b84b8b351f4
Dougal Rea <dougal.rea@echobox.com>
2022-09-20 16:07:55 +0100

    SL-2042 Ensure dev branch builds only deploy snapshot versions

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -22,8 +22,12 @@ if [ "$CIRCLE_BRANCH" == "$RELEASE_BRANCH" ] || [ "$CIRCLE_BRANCH" == "$DEV_BRAN
   printf "${GREEN_COLOUR}Performing deploy build to maven central.${NO_COLOUR}\n"
   echo "${GPG_SECRET_KEYS}" | base64 --decode | $GPG_EXECUTABLE --import --batch --passphrase "${GPG_PASSPHRASE}" || echo "Failed to import GPG_SECRET_KEYS."
   echo "${GPG_OWNERTRUST}" | base64 --decode | $GPG_EXECUTABLE --import-ownertrust --batch --passphrase "${GPG_PASSPHRASE}" || echo "Failed to import GPG_OWNERTRUST."
-  GPG_TTY=$(tty)
-  export GPG_TTY
+  
+  if [ "$CIRCLE_BRANCH" == "$DEV_BRANCH" ]; then
+    MVN_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout|grep -v '\[')
+    mvn versions:set -q -DnewVersion="$MVN_VERSION-SNAPSHOT"
+  fi
+
   mvn clean deploy --settings .maven.xml -B -U -Prelease
 else
   printf "${GREEN_COLOUR}Performing a PR verify build. Releases are only created from $DEV_BRANCH and $RELEASE_BRANCH branches.${NO_COLOUR}\n"
