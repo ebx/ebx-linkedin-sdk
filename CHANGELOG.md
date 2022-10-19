8c2b218c14ba52fccb53758c672667250ca5e3f0
Dercio Daio <33119595+mcadecio@users.noreply.github.com>
2022-10-19 12:38:31 +0100

    GH-123 Fixed unable to obtain accessToken (#135)
    * GH-123 Add application/x-wwww-form-urlenconded header to obtain access token request and update additional header method to add headers as a list.
    
    * GH-123 Updating change log
    
    * GH-123 Removing Work In Progress in changelog
    
    * GH-123 - Updated addHeadersToRequest method to iterate through entry set of additional headers
    
    Co-authored-by: Kenneth Wong <79833772+kennethwong-hc@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -132,4 +132,5 @@ can get data for both organization and organization brand pages.
   is received after a request to the LinkedIn API
 * Updated dependencies
 
-## 3.1.3 (Work In Progress)
+## 3.1.3 (Oct 17, 2022)
+* Add `application/x-wwww-form-urlenconded` header to obtain access token request and update additional header method to add headers as a list.
--- src/main/java/com/echobox/api/linkedin/client/DefaultWebRequestor.java
+++ src/main/java/com/echobox/api/linkedin/client/DefaultWebRequestor.java
@@ -629,15 +629,16 @@ public class DefaultWebRequestor implements WebRequestor {
     return new JsonHttpContent(new GsonFactory(), map);
   }
   
-  private void addHeadersToRequest(HttpRequest request, HttpHeaders httpHeaders,
-      Map<String, String> headers) {
+  private void addHeadersToRequest(HttpRequest request, HttpHeaders httpHeaders, Map<String, String> headers) {
     if (headers != null) {
       // Add any additional headers
-      for (String headerKey : headers.keySet()) {
-        if (!headerKey.equalsIgnoreCase("content-type")) {
-          httpHeaders.put(headerKey, headers.get(headerKey));
-        }
-      }
+        headers.entrySet()
+                .stream()
+                .filter(headerEntry -> {
+                  String lowerCaseHeaderName = headerEntry.getKey().toLowerCase();
+                  return !httpHeaders.containsKey(lowerCaseHeaderName);
+                })
+                .forEach(headerEntry -> httpHeaders.put(headerEntry.getKey(), headerEntry.getValue()));
     }
   
     request.setHeaders(httpHeaders);
