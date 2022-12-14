34efd96454a8a0e70011d331d7bfc6811695102a
kennethwong-hc <kennethcareerhk@gmail.com>
2022-12-14 16:11:50 +0000

    SL-3751 Create base class of versioned connections

---
+++ src/main/java/com/echobox/api/linkedin/connection/versioned/VersionedConnection.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.echobox.api.linkedin.connection.versioned;
+
+import com.echobox.api.linkedin.client.LinkedInClient;
+import com.echobox.api.linkedin.connection.ConnectionBase;
+import com.echobox.api.linkedin.version.Version;
+
+/**
+ * Base class for all connections using versioned API
+ * @author Kenneth Wong
+ */
+public abstract class VersionedConnection extends ConnectionBase {
+  
+  /**
+   * The query key.
+   */
+  protected static final String QUERY_KEY = "q";
+  
+  /**
+   * Instantiates a new connection base.
+   *
+   * @param linkedinClient the LinkedIn client
+   */
+  protected VersionedConnection(LinkedInClient linkedinClient) {
+    super(linkedinClient);
+    if (Version.VERSIONED != linkedinClient.getVersion()) {
+      throw new IllegalArgumentException("The version of linkedInClient is not VERSIONED");
+    }
+  }
+}
