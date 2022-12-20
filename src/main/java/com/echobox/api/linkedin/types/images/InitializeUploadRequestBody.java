d02d38b67316e01138042ef8e806781eb53903e4
sergioebx <112696233+sergioebx@users.noreply.github.com>
2022-12-20 09:24:10 +0000

    SL-3768 Create images connection with request/response (#251)
    Create images connection with request/response

---
+++ src/main/java/com/echobox/api/linkedin/types/images/InitializeUploadRequestBody.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.echobox.api.linkedin.types.images;
+
+import com.echobox.api.linkedin.jsonmapper.LinkedIn;
+import com.echobox.api.linkedin.types.urn.URN;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Register Upload Request body
+ * @see <a href="https://learn.microsoft.com/en-us/linkedin/marketing/integrations/community-management/shares/images-api#initialize-image-upload">Initialize an Image Upload</a>
+ * @author Sergio Abplanalp
+ */
+@RequiredArgsConstructor
+public class InitializeUploadRequestBody {
+  
+  @NonNull
+  @Setter
+  @Getter
+  @LinkedIn
+  private InitializeUploadRequest initializeUploadRequest;
+  
+  /**
+   * InitializeUploadRequest object
+   * @author Sergio Abplanalp
+   */
+  @RequiredArgsConstructor
+  public static class InitializeUploadRequest {
+  
+    /**
+     * The URN of the entity that owns this asset
+     */
+    @NonNull
+    @Setter
+    @Getter
+    @LinkedIn
+    private URN owner;
+  }
+}
