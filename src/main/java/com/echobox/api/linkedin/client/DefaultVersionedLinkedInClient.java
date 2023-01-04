5e4058ae01c19ee816ca534d659e1e485eb4498d
Sergio <sergio.abplanalp@echobox.com>
2023-01-04 15:20:58 +0000

    Code cleanup

--- src/main/java/com/echobox/api/linkedin/client/DefaultVersionedLinkedInClient.java
+++ src/main/java/com/echobox/api/linkedin/client/DefaultVersionedLinkedInClient.java
@@ -519,7 +519,7 @@ public class DefaultVersionedLinkedInClient extends BaseLinkedInClient
         binaryAttachments != null && !binaryAttachments.isEmpty());
 
     return makeRequestFull(fullEndpoint, executeAsPost, executeAsDelete, jsonBody,
-        defaultHeaders, binaryAttachments, parameters);
+        binaryAttachments, parameters);
   }
   
   /**
@@ -536,8 +536,6 @@ public class DefaultVersionedLinkedInClient extends BaseLinkedInClient
    *          {@code true} to add a special 'treat this request as a {@code DELETE}' parameter.
    * @param jsonBody
    *          The POST JSON body
-   * @param headers
-   *          The headers for the request
    * @param binaryAttachments
    *          A list of binary files to include in a {@code POST} request. Pass {@code null} if no
    *          attachment should be sent.
@@ -546,7 +544,7 @@ public class DefaultVersionedLinkedInClient extends BaseLinkedInClient
    * @return The JSON returned by LinkedIn for the API call.
    */
   protected String makeRequestFull(String fullEndpoint, final boolean executeAsPost,
-      final boolean executeAsDelete, Object jsonBody, Map<String, String> headers,
+      final boolean executeAsDelete, Object jsonBody,
       final List<BinaryAttachment> binaryAttachments, Parameter... parameters) {
     verifyParameterLegality(parameters);
     
@@ -567,15 +565,15 @@ public class DefaultVersionedLinkedInClient extends BaseLinkedInClient
       @Override
       public WebRequestor.Response makeRequest() throws IOException {
         if (executeAsDelete && !isHttpDeleteFallback()) {
-          return webRequestor.executeDelete(fullEndpoint + finalParameterString, headers);
+          return webRequestor.executeDelete(fullEndpoint + finalParameterString, defaultHeaders);
         } else {
           return executeAsPost
               ? webRequestor.executePost(fullEndpoint, parameterString,
               jsonBody == null ? null : jsonMapper.toJson(jsonBody, true),
-              headers,
+              defaultHeaders,
               binaryAttachments == null ? null
                   : binaryAttachments.toArray(new BinaryAttachment[binaryAttachments.size()]))
-              : webRequestor.executeGet(fullEndpoint + finalParameterString, headers);
+              : webRequestor.executeGet(fullEndpoint + finalParameterString, defaultHeaders);
         }
       }
     });
