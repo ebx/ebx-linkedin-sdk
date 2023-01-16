db730fd2ca5a2e7bb68f13a0b8a17cdf21b3e5d0
kennethwong-hc <kennethcareerhk@gmail.com>
2023-01-16 16:24:36 +0000

    SL-3779 Update access level of no args constructor

--- src/main/java/com/echobox/api/linkedin/types/images/InitializeUploadRequestBody.java
+++ src/main/java/com/echobox/api/linkedin/types/images/InitializeUploadRequestBody.java
@@ -19,6 +19,7 @@ package com.echobox.api.linkedin.types.images;
 
 import com.echobox.api.linkedin.jsonmapper.LinkedIn;
 import com.echobox.api.linkedin.types.urn.URN;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -31,7 +32,7 @@ import lombok.Setter;
  * @author Sergio Abplanalp
  */
 @RequiredArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class InitializeUploadRequestBody {
   
   @NonNull
@@ -45,7 +46,7 @@ public class InitializeUploadRequestBody {
    * @author Sergio Abplanalp
    */
   @RequiredArgsConstructor
-  @NoArgsConstructor
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
   public static class InitializeUploadRequest {
   
     /**
