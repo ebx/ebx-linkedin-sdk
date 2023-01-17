5c39141e6632b7c6dfd7a4f0a9f1a9978d65632d
Sergio <sergio.abplanalp@echobox.com>
2023-01-17 15:13:39 +0000

    Remove unused imports

--- src/main/java/com/echobox/api/linkedin/client/DefaultWebRequestor.java
+++ src/main/java/com/echobox/api/linkedin/client/DefaultWebRequestor.java
@@ -55,7 +55,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
